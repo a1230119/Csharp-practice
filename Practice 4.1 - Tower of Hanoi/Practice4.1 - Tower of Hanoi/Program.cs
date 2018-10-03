86836957382817df713a3a9a59ad2d7d27cac251
a1230119 <42170360+a1230119@users.noreply.github.com>
2018-10-03 13:32:52 +0800

    Add files via upload

---
+++ Practice 4.1 - Tower of Hanoi/Practice4.1 - Tower of Hanoi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4._1___Tower_of_Hanoi
+{
+    class Program
+    {
+        private static void Hanoi(int layer, char x, char y, char z)
+        {
+            if (layer == 1)
+                Console.WriteLine("Move sheet from {0} to {1}", x, z);
+            else
+            {
+                Hanoi(layer - 1, x, z, y);
+                Console.WriteLine("Move sheet from {0} to {1}", x, z);
+                Hanoi(layer - 1, y, x, z);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入盤數 : ");
+            int layer = int.Parse(Console.ReadLine());
+            Hanoi(layer, 'A', 'B', 'C');
+            Console.Read();
+        }
+    }
+}
